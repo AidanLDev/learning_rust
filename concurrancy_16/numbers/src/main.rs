8dd8379504edfc0cd0d112ed19ed28b62a15fa7b
Aidan Lowson <dev@aidanlowson.com>
2025-01-23 17:44:45 +0000

    Adding vector example

--- concurrancy_16/numbers/src/main.rs
+++ concurrancy_16/numbers/src/main.rs
@@ -2,6 +2,8 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
+    /*
+     // thread iteration example
     let handle = thread::spawn(|| {
         for i in 1..10 {
             println!("Hi number {i} from the spawned thread!");
@@ -12,9 +14,18 @@ fn main() {
 
     for i in 1..5 {
         println!("Hi number {i} from the main thread!");
-
         thread::sleep(Duration::from_millis(1));
     }
 
     handle.join().unwrap();
+    */
+
+    // Thread with vector example
+    let v = vec![1, 2, 3,];
+
+    let handle = thread::spawn(move|| {
+        println!("Here's a vector: {:?}", v);
+    });
+
+    handle.join().unwrap()
 }
