35bb847b9feabfdd8b870a7072d376f5d654311a
Aidan Lowson <dev@aidanlowson.com>
2025-01-23 07:48:34 +0000

    Adding join to make sure spawned thread finishes before exit

--- concurrancy_16/numbers/src/main.rs
+++ concurrancy_16/numbers/src/main.rs
@@ -2,7 +2,7 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    thread::spawn(|| {
+    let handle = thread::spawn(|| {
         for i in 1..10 {
             println!("Hi number {i} from the spawned thread!");
 
@@ -15,4 +15,6 @@ fn main() {
 
         thread::sleep(Duration::from_millis(1));
     }
+
+    handle.join().unwrap();
 }
