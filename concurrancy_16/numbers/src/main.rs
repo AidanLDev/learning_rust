4d3bfdc62c9f23f4b269b74c10e5b67ace05afe5
Aidan Lowson <dev@aidanlowson.com>
2025-01-25 10:44:06 +0000

    Adding vec

--- concurrancy_16/numbers/src/main.rs
+++ concurrancy_16/numbers/src/main.rs
@@ -35,9 +35,16 @@ fn main() {
     let (tx, rx) = mpsc::channel();
 
     thread::spawn(move || {
-        let val = String::from("hi");
-
-        tx.send(val).unwrap();
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
     });
     let recieved = rx.recv().unwrap();
 
